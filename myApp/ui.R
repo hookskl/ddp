a9c25d7b1e2162231f50a4a41e0bd42c9d16742e
Kyle <klhooks71@gmail.com>
2021-10-12 10:58:18 -0400

    shiny app and markdown

--- myApp/ui.R
+++ myApp/ui.R
@@ -1,33 +1,19 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
+    titlePanel("Predict Horsepower from MPG"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("sliderMPG", "What is the MPG of the car?", 10, 35, value = 20),
+            checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
+            checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE),
+            submitButton("Submit") 
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotOutput("plot1"),
+            h3("Predicted Horsepower from Model 1:"),
+            textOutput("pred1"),
+            h3("Predicted Horsepower from Model 2:"),
+            textOutput("pred2")
         )
     )
 ))
